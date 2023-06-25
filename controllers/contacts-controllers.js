4897f7586e82bffd08d20c1479e591bd82acb289
KsenyaKoss <roxenn34@gmail.com>
2023-06-25 09:50:02 +0300

    FIXED

--- controllers/contacts-controllers.js
+++ controllers/contacts-controllers.js
@@ -6,11 +6,9 @@ const { HttpError } = require("../helpers");
 
 
 const listContacts = async (req, res) => {
-    const {_id: owner} = req.user
-    const {page = 1, limit = 20} = req.params;
-    const skip = (page - 1) * limit
-    const contacts = await Contact.find({owner}, {skip, limit}).populate('owner', 'name email');
-    res.json(contacts);
+    const {_id: owner} = req.user;
+    const result = await Contact.find({owner}).populate('owner', 'name email');
+    res.json(result);
  
 }
 
